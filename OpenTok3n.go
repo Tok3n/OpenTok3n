43de76195ebe36dd8a083a4a9916f40bf1e8674d
Jose Angel Espinoza Portillo <jose@ngel.es>
2013-10-30 18:51:49 -0600

    Ennabling custom host and port servers. And a static address for static files

--- OpenTok3n.go
+++ OpenTok3n.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"flag"
+	"log"
+	"net/http"
+	)
 
+
+var port = flag.String("port", "8080", "server port")
+var addr = flag.String("addr", "localhost", "http service address") // Q=17, R=18
 func main() {
-	fmt.Printf("Hello, OpenTok3n Just for the lols.\n")
+	flag.Parse()
+
+	var localUrl = fmt.Sprintf("%s:%s",*addr,*port)
+	log.Print(localUrl)
+
+
+	http.Handle("/", http.HandlerFunc(initRoot))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./src/github.com/Tok3n/OpenTok3n/webResources/static/"))))
+	
+	err := http.ListenAndServe(localUrl, nil)
+    if err != nil {
+        log.Fatal("ListenAndServe:", err)
+    }
+}
+
+func initRoot(w http.ResponseWriter, req *http.Request) {
+    fmt.Fprintf(w,"Hello Open Tok3n, Just for the lols at port: %s",*port)
 }
