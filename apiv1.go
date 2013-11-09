cad69e8a815e3741d2eb7c88edab53474a66a2ca
Jose Angel Espinoza Portillo <jose@ngel.es>
2013-11-09 17:23:57 -0600

    need the callbackdata to sincronice back. if not provided an integration error is shown

--- apiv1.go
+++ apiv1.go
@@ -17,6 +17,17 @@ func registerApiHandlers(){
 var TOK3N_DOMAIN = "my.tok3n.com"
 
 func getNewUserSession_API(w http.ResponseWriter, req *http.Request){
+	data := req.FormValue("callbackdata")
+	if data == ""{
+		cod,err := qr.Encode(fmt.Sprintf("http://%s/api/v1/openTok3n/getIntegrationError",TOK3N_DOMAIN),qr.M)
+		if err != nil{
+			fmt.Fprintf(w,"Something went very wrong, verify OpenTok3n integration instalation")
+			return
+		}
+		img := cod.PNG()
+		w.Write(img)
+		return
+	}
 	resp, err := http.PostForm(fmt.Sprintf("http://%s/api/v1/openTok3n/getNewUserSession",TOK3N_DOMAIN) , url.Values{"kind": {"OTaccess"}, "secretKey": {configData.Tok3nAPISecret}})
 	if err != nil{
 		cod,err := qr.Encode(fmt.Sprintf("http://%s/api/v1/openTok3n/getIntegrationError",TOK3N_DOMAIN),qr.M)
@@ -30,6 +41,9 @@ func getNewUserSession_API(w http.ResponseWriter, req *http.Request){
 	}
 	defer resp.Body.Close()
 	body, _ := ioutil.ReadAll(resp.Body)
+
+	addTok3nAuthenticationSeccion(string(body),data)
+
 	cod,err := qr.Encode(fmt.Sprintf("http://%s/api/v1/openTok3n/validateNewUserSession?session=%s&key=%s",TOK3N_DOMAIN,body,configData.Tok3nAPIKey),qr.H)
 	if err != nil{
 		fmt.Fprintf(w,"Something whent very wrong, verify OpenTok3n integration instalation")
