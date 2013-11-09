1222414c9a4961c631827d5387b486fbec623034
Jose Angel Espinoza Portillo <jose@ngel.es>
2013-11-09 17:40:46 -0600

    Adding parameters for model of the new user session

--- datamodel.go
+++ datamodel.go
@@ -1,10 +1,31 @@
 package main
 import (
+	"fmt"
+	"time"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-//
+/**
+Session
+**/
 
+type ApiSesion struct{
+	IdVal	int
+    ImplementationUser int
+	SesionID	string
+	Creation	time.Time
+	Used	time.Time
+    Kind int // 1:auth; 2:access; 3:Tok3naccess
+    Status int //auth{0:issued,1:valid,2:invalid}
+    Data string
+}
 
-
-
+func addTok3nAuthenticationSeccion(session string,data string)error{
+	db,err := _getDB()
+	if err != nil{
+		return err
+	}
+	err = _DBExcecute(db,fmt.Sprintf("INSERT INTO ApiSesion VALUES (0,0,'%s',now(),NULL,3, -1, '%s')",session,data))
+	db.Close()
+	return err
+}
