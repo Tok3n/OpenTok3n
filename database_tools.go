a6fb6d8c0f45553d982a3319300aeff9dd2e61f9
Jose Angel Espinoza Portillo <jose@ngel.es>
2013-11-09 22:03:03 -0600

    Ennabling parsing time

--- database_tools.go
+++ database_tools.go
@@ -10,7 +10,7 @@ import (
 	Database tools
 **/
 func _getDB()(*sql.DB, error){
-	return sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",configData.DBUser, configData.DBPassword,configData.DBAddress,configData.DBPort,configData.DBName))
+	return sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",configData.DBUser, configData.DBPassword,configData.DBAddress,configData.DBPort,configData.DBName))
 }
 
 func _DBExcecute(db *sql.DB,query string) error{
